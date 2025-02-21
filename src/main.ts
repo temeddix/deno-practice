b92cad6bc11501446ac9d3ee543790004e29e51d
Donghyun Kim <temeddix@gmail.com>
2025-02-21 15:26:19 +0900

    Organize TypeScript files

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,5 @@
-import "./web_component.ts";
-
-function add(a: number, b: number): number {
-  return a + b;
-}
+import "@src/web_component.ts";
+import { add } from "@src/logic.ts";
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 console.log("Add 2 + 3 =", add(2, 3));
